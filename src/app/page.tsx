7f6cc4069857c7918cb18eaa2a5a08bf28b13c86
hamed <hamed7087@gmail.com>
2026-01-07 21:32:45 +0330

    x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function RootPage() {
-  redirect("/en"); // یا fa
+  return <p className="bg-red-600 h-[600px] w-full">fvf dfvdf sdc d</p>;
 }
